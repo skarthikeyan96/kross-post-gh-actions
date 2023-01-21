902bafbf170b5b3726bacda7ff42801c06bd82e0
Karthikeyan Shanmugam <karthikeyan@bangthetable.com>
2023-01-21 23:40:42 +0530

    fix: adding a sample md file

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-import core from '@actions/core'
-
+const core = require('@actions/core')
 require('dotenv').config()
 
 // get the latest file from the post directory
 // then setup
+
+const files = core.getInput('files')
+console.log(files)
