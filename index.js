7d12377f9f312c0888f456bf700bcca5fc54727f
Karthikeyan Shanmugam <karthikeyan@bangthetable.com>
2023-01-22 14:35:08 +0530

    fix: update input args

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const core = require('@actions/core')
 require('dotenv').config()
 
-// get the latest file from the post directory
-// then setup
 
 const files = core.getInput('files')
 console.log(files)
